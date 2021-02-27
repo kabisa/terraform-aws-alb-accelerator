f018f69254577a82fcb5a1b55382ca03b357a966
Joost Saanen <joost@kabisa.nl>
2021-02-27 21:41:07 +0100

    add port 80

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 resource "aws_security_group" "https" {
   vpc_id      = var.vpc_id
-  name        = "HTTPS sg"
+  name        = "${var.alb_name}-sg"
   description = "Allow HTTPS traffic"
 
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
